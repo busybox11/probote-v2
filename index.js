3800b57f3482bb700b207c4bf56850a4a708d558
busybox11 <planetgeek11@gmail.com>
2020-09-01 10:38:18 +0200

    Upgrades, upgrades, upgrades!

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const pronote = require('./pronote/index');
-const { enable_discord } = require('./config');
+const pronote_api = require('pronote-api');
 
-if (enable_discord == 'true') {
-    const discord = require("./clients/discord/index");
-}
+pronote.logIn().catch(err => {
+    if (err.code === pronote_api.errors.WRONG_CREDENTIALS.code) {
+        console.error('Mauvais identifiants');    
+    } else {
+        console.error(err);
+    }
+    process.exit(1);
+}).then(function () {
+    const { enable_discord } = require('./config');
+
+    if (enable_discord == 'true') {
+        const discord = require('./clients/discord/index');
+    }
+})

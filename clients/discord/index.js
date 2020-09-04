b06b3de018cf5de6fc4b541bb5a7e3253af2d452
busybox11 <planetgeek11@gmail.com>
2020-09-05 00:29:43 +0200

    Added presence with school name

--- clients/discord/index.js
+++ clients/discord/index.js
@@ -1,10 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const { session } = require('../../config');
+
 const { discord_bot_token } = require('../../config');
 
 client.on('ready', () => {
     console.log("[DISCORD] Logged in as " + client.user.tag)
+    client.user.setActivity(session.user.establishment, { type: 'WATCHING' });
 });
 
 module.exports = { client }

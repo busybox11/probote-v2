4ee3653ccb6a358aa0fbbe459a1c793c10a38dbe
busybox11 <planetgeek11@gmail.com>
2020-09-18 08:46:13 +0200

    Handle empty menu case

--- pronote/commands/menu.js
+++ pronote/commands/menu.js
@@ -8,28 +8,37 @@ async function getMenu() {
 async function runDiscord() {
     menu = await getMenu();
 
-    msg = {
-        useEmbed: true,
-        embed: {
-            title: 'Menu de la cantine',
-            timestamp: menu[0].date,
-            fields: []
+    if (menu[0] !== undefined) {
+        msg = {
+            useEmbed: true,
+            embed: {
+                title: 'Menu de la cantine',
+                timestamp: menu[0].date,
+                fields: []
+            }
         }
+    
+        menu[0].meals[0].forEach(function(item){
+            let listItems = '';
+            item.forEach(function(mealItem){
+                listItems += mealItem.name + '\n';
+            });
+    
+            msg.embed.fields.push({
+                name: '** **',
+                value: listItems
+            });
+        });
+
+        return msg;
     }
 
-    menu[0].meals[0].forEach(function(item){
-        let listItems = '';
-        item.forEach(function(mealItem){
-            listItems += mealItem.name + '\n';
-        });
-
-        msg.embed.fields.push({
-            name: '** **',
-            value: listItems
-        });
-    });
-
-    return msg;
+    else {
+        return {
+            useEmbed: false,
+            content: 'Aucun menu pour la période demandée.'
+        }
+    }
 }
 
 module.exports = {

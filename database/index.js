ebca9e44de399e11a573c89219ba4d7b807c0159
busybox <planetgeek11@gmail.com>
2020-09-19 12:25:55 +0000

    Added database methods

--- database/index.js
+++ database/index.js
@@ -3,11 +3,21 @@ const fs = require('fs');
 let general, homework, grades, infos, timetable;
 
 function getDB(dbname) {
-    return eval(dbname);
+    let dbcontent;
+    try {
+        dbcontent = JSON.parse(fs.readFileSync(`database/files/${dbname}.json`));
+    } catch (error) {
+        console.error("[DATABASE] Error while reading data\n", err);
+    }
+    return dbcontent;
 }
 
 function setDB(dbname, dbvar) {
-    eval(dbname + " = dbvar;");
+    try {
+        fs.writeFileSync(`database/files/${dbname}.json`, JSON.stringify(dbvar));
+    } catch (error) {
+        console.error("[DATABASE] Error while saving data\n", err);
+    }
 }
 
 module.exports = {

2df7af6e3f31c061729a62fd2570adc073ba5335
busybox <planetgeek11@gmail.com>
2020-09-26 21:51:02 +0000

    Added pro note command

--- pronote/commands/menu.js
+++ pronote/commands/menu.js
@@ -21,11 +21,12 @@ async function runDiscord() {
             }
         }
     
-        menu[0].meals[0].forEach(function(item){
+        menu[0].meals[0].forEach(function(item) {
             let listItems = '';
             item.forEach(function(mealItem){
                 listItems += mealItem.name + '\n';
-            });
+            }
+        );
     
             msg.embed.fields.push({
                 name: '** **',
---
+++ pronote/notifications/notes.js
@@ -0,0 +1,56 @@
+async function getMarks() {
+    // MARKS
+    // Regular notes (e.g. 14.62)    
+    let { session } = require('../../config');
+    const marks = await session.marks();
+
+    return marks;
+}
+
+/*
+ * async function getEvaluations() {
+ *     // EVALUATIONS
+ *     // Level-based notes (e.g. expert - aka Green +)
+ *     let { session } = require('../../config');
+ *     const evaluations = await session.evaluations();
+ * 
+ *     return evaluations;
+ * }
+ */
+
+async function runDiscord() {
+    marks = await getMarks();
+    // evaluations = await getEvaluations();
+
+    console.log(marks);
+    // console.log(evaluations);
+
+    msg = {
+        useEmbed: true,
+        embed: {
+            title: 'Notes du trimestre actuel',
+            fields: []
+        }
+    }
+
+    marks.subjects.forEach(function(item) {
+        msg.embed.fields.push({
+            name: `**${item.name}**`,
+            value: `Moyenne de la classe : **${item.averages.studentClass}**\nMoyenne max : **${item.averages.max}**\nMoyenne min : **${item.averages.min}**`
+        });
+    });
+
+    return msg;
+}
+
+async function autoFetch() {
+    console.log('[NOTES] Nothing to send');
+}
+
+module.exports = {
+    name: "Notes",
+    desc: "Module lié aux notes",
+    usage: "note",
+    runDiscord,
+    autoFetch
+}
